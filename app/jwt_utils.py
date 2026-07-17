54fa9d8f4fe750775671bec374351be7446e9f93
rittripathi <tripathirit@gmail.com>
2026-07-17 20:10:48 +0530

    checking

--- app/jwt_utils.py
+++ app/jwt_utils.py
@@ -12,5 +12,5 @@ def create_access_token(data:dict)->str:
     to_encode=data.copy()
     expire= datetime.utcnow()+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)edfwaesf
 
